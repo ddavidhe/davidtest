baf05d3390800e9ccfe0ddd86d2356b1ff6b8426
ddavidhe <davidhe.3612@gmail.com>
2023-06-23 18:52:55 -0400

    now displays apples upon button press

---
+++ src/components/fetchFood.jsx
@@ -0,0 +1,25 @@
+const getApple = async () => {
+    try {
+      const response = await fetch("https://api.edamam.com/api/food-database/v2/parser?app_id=05937eba&app_key=7f8c81b2f89b7d71d740c4ba140c9201&ingr=apple&nutrition-type=cooking&category=generic-foods");
+      const data = await response.json();
+  
+      const food = data.parsed[0].food;
+      const nutrients = food.nutrients;
+      const nutrientsObj = {
+        name: food.label,
+        calories: nutrients.ENERC_KCAL,
+        protein: nutrients.PROCNT,
+        fat: nutrients.FAT,
+        carbohydrates: nutrients.CHOCDF,
+        fiber: nutrients.FIBTG
+      };
+  
+      return nutrientsObj;
+    } catch (error) {
+      console.log("Error fetching data:", error);
+      throw error;
+    }
+  };
+  
+  export default getApple;
+  
--- src/main.jsx
+++ src/main.jsx
@@ -1,40 +1,49 @@
-//import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import logo from './logo.svg';
 import './main.css';
-
-
+import getApple from './components/fetchFood';
 
 function App() {
+  const [nutrientsObj, setNutrientsObj] = useState(null);
 
-  const nutrientsObj = {}
-
-  const getApple = () => {
-    fetch("https://api.edamam.com/api/food-database/v2/parser?app_id=05937eba&app_key=7f8c81b2f89b7d71d740c4ba140c9201&ingr=apple&nutrition-type=cooking&category=generic-foods")
-      .then((res) => res.json())
-      .then((data) => {
-        let nutrients = data.parsed[0].food.nutrients;
-        nutrientsObj.calories = nutrients.ENERC_KCAL;
-        nutrientsObj.protein = nutrients.PROCNT;
-        nutrientsObj.fat = nutrients.FAT;
-        nutrientsObj.carbohydrates = nutrients.CHOCDF;
-        nutrientsObj.fiber = nutrients.FIBTG;
-        console.log(nutrientsObj);
-      })
-      .catch((error) => {
-        console.log("Error fetching data:", error);
-      });
+  const handleClick = async () => {
+    try {
+      const data = await getApple();
+      setNutrientsObj(data);
+    } catch (error) {
+      // Handle error if necessary
+    }
   };
-  
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={() => getApple()}>
-          button to console.log the info
-        </p>
-        <h1>
-          cals
-        </h1>
+        <button onClick={handleClick}>
+          Button to fetch and display the info
+        </button>
+        {nutrientsObj && (
+          <>
+            <div>
+              <b>Name:</b> {nutrientsObj.name}
+            </div>
+            <div>
+              <b>Calories:</b> {nutrientsObj.calories}
+            </div>
+            <div>
+              <b>Protein:</b> {nutrientsObj.protein}
+            </div>
+            <div>
+              <b>Fat:</b> {nutrientsObj.fat}
+            </div>
+            <div>
+              <b>Carbohydrates:</b> {nutrientsObj.carbohydrates}
+            </div>
+            <div>
+              <b>Fiber:</b> {nutrientsObj.fiber}
+            </div>
+          </>
+        )}
       </header>
     </div>
   );
