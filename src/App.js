ad6716c77a902b451d4f0911c072c040d2b4df41
decfinla <decfinla@uwaterloo.ca>
2023-06-27 15:31:41 -0400

    Home button is in

--- src/App.js
+++ src/App.js
@@ -5,25 +5,50 @@ import GetVegetables from "./components/GetVegetables";
 import GetGrains from "./components/GetGrains";
 import GetDairy from "./components/GetDairy";
 import GetMeat from "./components/GetMeat";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Outlet } from "react-router-dom";
 import NoPage from "./components/NoPage";
+import { useNavigate } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <div className="website-banner">Snack Track!</div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<SearchFood />} />
-          <Route path="/fruits" element={<GetFruits />} />
-          <Route path="/vegetables" element={<GetVegetables />} />
-          <Route path="/grains" element={<GetGrains />} />
-          <Route path="/dairy" element={<GetDairy />} />
-          <Route path="/meat" element={<GetMeat />} />
-          <Route path="*" element={<NoPage />} />
+          <Route path="/" element={<Layout />}>
+            <Route index element={<SearchFood />} />
+            <Route path="/fruits" element={<GetFruits />} />
+            <Route path="/vegetables" element={<GetVegetables />} />
+            <Route path="/grains" element={<GetGrains />} />
+            <Route path="/dairy" element={<GetDairy />} />
+            <Route path="/meat" element={<GetMeat />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
         </Routes>
       </BrowserRouter>
     </div>
+  );
+}
+
+function Layout() {
+  let navigate = useNavigate();
+
+  function RouteHome() {
+    navigate("/");
+  }
+
+  return (
+    <>
+      <div className="website-banner">
+        Snack Track
+        <br></br>
+        <button className="categories-button" onClick={RouteHome}>
+          <span class="text">Home</span>
+        </button>
+      </div>
+      <div>
+        <Outlet />
+      </div>
+    </>
   );
 }
 
--- src/components/GetDairy.js
+++ src/components/GetDairy.js
@@ -4,19 +4,7 @@ import { useNavigate } from "react-router-dom";
 function GetDairy() {
   let navigate = useNavigate();
 
-  function RouteHome() {
-    navigate("/");
-  }
-
-  return (
-    <>
-      <div>
-        <button className="categories-button-home" onClick={RouteHome}>
-          <span class="text">Home</span>
-        </button>
-      </div>
-    </>
-  );
+  return <></>;
 }
 
 export default GetDairy;
